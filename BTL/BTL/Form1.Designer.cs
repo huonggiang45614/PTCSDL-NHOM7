0e0f0a25e98c43d66ce53bd01d873dc3970b7ce0
hmeiz <mai96950@st.vimaru.edu.vn>
2024-12-04 14:36:46 +0700

    Add files via upload

---
+++ BTL/BTL/Form1.Designer.cs
@@ -0,0 +1,254 @@
+﻿namespace BTL
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btthem = new System.Windows.Forms.Button();
+            this.tbidnhacungcap = new System.Windows.Forms.TextBox();
+            this.btxoa = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbdiachi = new System.Windows.Forms.TextBox();
+            this.btsua = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbtennhacungcap = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbsdt = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbtimkiem = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label5 = new System.Windows.Forms.Label();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btthem);
+            this.groupBox1.Controls.Add(this.tbidnhacungcap);
+            this.groupBox1.Controls.Add(this.btxoa);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.tbdiachi);
+            this.groupBox1.Controls.Add(this.btsua);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.tbtennhacungcap);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.tbsdt);
+            this.groupBox1.Location = new System.Drawing.Point(32, 26);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(735, 217);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // btthem
+            // 
+            this.btthem.Location = new System.Drawing.Point(90, 149);
+            this.btthem.Name = "btthem";
+            this.btthem.Size = new System.Drawing.Size(108, 40);
+            this.btthem.TabIndex = 16;
+            this.btthem.Text = "Thêm";
+            this.btthem.UseVisualStyleBackColor = true;
+            this.btthem.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tbidnhacungcap
+            // 
+            this.tbidnhacungcap.Enabled = false;
+            this.tbidnhacungcap.Location = new System.Drawing.Point(184, 34);
+            this.tbidnhacungcap.Name = "tbidnhacungcap";
+            this.tbidnhacungcap.Size = new System.Drawing.Size(194, 26);
+            this.tbidnhacungcap.TabIndex = 13;
+            // 
+            // btxoa
+            // 
+            this.btxoa.Location = new System.Drawing.Point(508, 149);
+            this.btxoa.Name = "btxoa";
+            this.btxoa.Size = new System.Drawing.Size(108, 40);
+            this.btxoa.TabIndex = 14;
+            this.btxoa.Text = "Xóa";
+            this.btxoa.UseVisualStyleBackColor = true;
+            this.btxoa.Click += new System.EventHandler(this.btxoa_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(420, 97);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(106, 20);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Số điện thoại:";
+            // 
+            // tbdiachi
+            // 
+            this.tbdiachi.Location = new System.Drawing.Point(535, 34);
+            this.tbdiachi.Name = "tbdiachi";
+            this.tbdiachi.Size = new System.Drawing.Size(162, 26);
+            this.tbdiachi.TabIndex = 12;
+            // 
+            // btsua
+            // 
+            this.btsua.Location = new System.Drawing.Point(303, 149);
+            this.btsua.Name = "btsua";
+            this.btsua.Size = new System.Drawing.Size(108, 40);
+            this.btsua.TabIndex = 15;
+            this.btsua.Text = "Sửa";
+            this.btsua.UseVisualStyleBackColor = true;
+            this.btsua.Click += new System.EventHandler(this.btsua_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(420, 37);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Địa chỉ:";
+            // 
+            // tbtennhacungcap
+            // 
+            this.tbtennhacungcap.Location = new System.Drawing.Point(184, 94);
+            this.tbtennhacungcap.Name = "tbtennhacungcap";
+            this.tbtennhacungcap.Size = new System.Drawing.Size(194, 26);
+            this.tbtennhacungcap.TabIndex = 11;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(130, 20);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "ID nhà cung cấp:";
+            // 
+            // tbsdt
+            // 
+            this.tbsdt.Location = new System.Drawing.Point(535, 94);
+            this.tbsdt.Name = "tbsdt";
+            this.tbsdt.Size = new System.Drawing.Size(162, 26);
+            this.tbsdt.TabIndex = 9;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(56, 284);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(141, 20);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Thông tin tìm kiếm:";
+            // 
+            // tbtimkiem
+            // 
+            this.tbtimkiem.Location = new System.Drawing.Point(206, 278);
+            this.tbtimkiem.Name = "tbtimkiem";
+            this.tbtimkiem.Size = new System.Drawing.Size(523, 26);
+            this.tbtimkiem.TabIndex = 11;
+            this.tbtimkiem.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.dataGridView1);
+            this.groupBox2.Location = new System.Drawing.Point(32, 330);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(729, 263);
+            this.groupBox2.TabIndex = 12;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "groupBox2";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(6, 38);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(723, 214);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(24, 97);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(140, 20);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Tên nhà cung cấp:";
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(825, 679);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.tbtimkiem);
+            this.Controls.Add(this.label2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btthem;
+        private System.Windows.Forms.TextBox tbidnhacungcap;
+        private System.Windows.Forms.Button btxoa;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbdiachi;
+        private System.Windows.Forms.Button btsua;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbtennhacungcap;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbsdt;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbtimkiem;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+    }
+}
+
---
+++ BTL/BTL/Form1.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace BTL
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection connection;
+        SqlCommand command;
+        string str = "Data Source=LAPTOP-L56TLFGS;Initial Catalog=QLCHQA;User ID=sa;Password=123123123";
+        SqlDataAdapter adapter = new SqlDataAdapter();
+        DataTable table=new DataTable();
+        void loaddata()
+        {
+            command = connection.CreateCommand();
+            command.CommandText = "SELECT*FROM NHACUNGCAP";
+            adapter.SelectCommand = command;
+            table.Clear();
+            adapter.Fill(table);
+            dataGridView1.DataSource = table;
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            connection=new SqlConnection(str);
+            connection.Open();
+            loaddata();
+        }
+        private bool ckform()
+        {
+            bool isFine = true;
+            errorProvider1.Clear();
+            if (tbtennhacungcap.Text.Trim() == "")
+            {
+                errorProvider1.SetError(tbtennhacungcap, "Chưa nhập tên nhà cung cấp");
+                isFine = false;
+            }
+            if (tbdiachi.Text.Trim() == "")
+            {
+                errorProvider1.SetError(tbdiachi, "Chưa nhập địa chỉ nhà cung cấp");
+                isFine= false;
+            }
+            if(tbsdt.TextLength != 10 || !tbsdt.Text.All(Char.IsDigit))
+            {
+                errorProvider1.SetError(tbsdt, "Chưa nhập số điện thoại");
+                isFine = false;
+            }
+            return isFine;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!ckform()) return;
+            try
+            {
+                // chuẩn hóa tên 
+                tbtennhacungcap.Text = ChuanHoaTen(tbtennhacungcap.Text);
+                tbdiachi.Text = ChuanHoaTen(tbdiachi.Text);
+                // ktr nếu tên tồn tại
+                command = connection.CreateCommand();
+                command.CommandText = "SELECT*FROM NHACUNGCAP WHERE TENNHACUNGCAP=N'" + tbtennhacungcap.Text + "' AND DIACHI=N'" + tbdiachi.Text + "' AND DIENTHOAI='" + tbsdt.Text + "'";
+                SqlDataReader reader = command.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    errorProvider1.SetError(tbtennhacungcap, "Đã có nhà cung cấp này");
+                    reader.Close();
+                    return;
+                }
+                reader.Close();
+
+
+                command = connection.CreateCommand();
+                command.CommandText = "INSERT INTO NHACUNGCAP VALUES(N'" + tbtennhacungcap.Text + "',N'" + tbdiachi.Text + "','" + tbsdt.Text + "')";
+                command.ExecuteNonQuery();
+                loaddata();
+                MessageBox.Show("Thêm thành công!");
+                cleartb();
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show("Có lỗi khi thêm dữ liệu." + ex.Message, "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Có lỗi không xác định." + ex.Message, "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int i;
+            i = dataGridView1.CurrentRow.Index;
+            tbidnhacungcap.Text = dataGridView1.Rows[i].Cells[0].Value.ToString();
+            tbtennhacungcap.Text = dataGridView1.Rows[i].Cells[1].Value.ToString();
+            tbdiachi.Text = dataGridView1.Rows[i].Cells[2].Value.ToString();
+            tbsdt.Text = dataGridView1.Rows[i].Cells[3].Value.ToString();
+        }
+        private void cleartb()
+        {
+            tbidnhacungcap.Clear();
+            tbtennhacungcap.Clear();
+            tbdiachi.Clear();
+            tbsdt.Clear();
+        }
+        private string ChuanHoaTen(string ten)
+        { // Tách các từ trong chuỗi tên
+           string[] tu = ten.Trim().Split(' '); 
+            // Chuẩn hóa từng từ
+           for (int i = 0; i < tu.Length; i++) 
+            { 
+                if (!string.IsNullOrEmpty(tu[i])) 
+                { 
+                    tu[i] = char.ToUpper(tu[i][0]) + tu[i].Substring(1).ToLower(); 
+                } 
+            } // Kết hợp lại thành chuỗi hoàn chỉnh
+            return string.Join(" ", tu);
+        }
+            private void btsua_Click(object sender, EventArgs e)
+        {
+            if (!ckform())return;
+            try
+            {
+                //chuẩn hóa tên
+                tbtennhacungcap.Text = ChuanHoaTen(tbtennhacungcap.Text);
+                // ktr nếu tên tồn tại
+                command = connection.CreateCommand();
+                command.CommandText = "SELECT*FROM NHACUNGCAP WHERE TENNHACUNGCAP=N'" + tbtennhacungcap.Text + "' ";
+                SqlDataReader reader = command.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    errorProvider1.SetError(tbtennhacungcap, "Đã có nhà cung cấp này");
+                    reader.Close();
+                    return;
+                }
+                reader.Close();
+              
+                // Kiểm tra nếu số điện thoại trùng (ngoại trừ chính nó)
+                command = connection.CreateCommand();
+                command.CommandText = "SELECT * FROM NHACUNGCAP WHERE DIENTHOAI = '" + tbsdt.Text + "' AND IDNHACUNGCAP <> '" + tbidnhacungcap.Text + "'"; 
+                SqlDataReader reader1 = command.ExecuteReader();
+                if (reader1.HasRows) 
+                { 
+                    errorProvider1.SetError(tbsdt, "Đã có số điện thoại này");
+                    reader1.Close();
+                    return;
+                }
+                reader.Close();
+                command = connection.CreateCommand();
+                command.CommandText = "UPDATE NHACUNGCAP SET TENNHACUNGCAP='" + tbtennhacungcap.Text + "',DIACHI='" + tbdiachi.Text + "',DIENTHOAI='" + tbsdt.Text + "' WHERE IDNHACUNGCAP='" + tbidnhacungcap.Text + "'";
+                command.ExecuteNonQuery();
+                loaddata();
+                MessageBox.Show("Cập nhật thành công!");
+                cleartb();
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show("Có lỗi khi sửa dữ liệu." + ex.Message, "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Có lỗi không xác định." + ex.Message, "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btxoa_Click(object sender, EventArgs e)
+        {
+            if (!ckform()) return;
+            try
+            {
+                command = connection.CreateCommand();
+                command.CommandText = "DELETE FROM CHITIETDATHANG WHERE IDMATHANG IN (SELECT IDMATHANG FROM MATHANG WHERE IDNHACUNGCAP = '"+tbidnhacungcap.Text+"')";
+                command.ExecuteNonQuery();
+                command.CommandText = "DELETE FROM MATHANG WHERE IDNHACUNGCAP = '"+tbidnhacungcap.Text+"'";
+                command.ExecuteNonQuery();
+                command.CommandText = "DELETE FROM NHACUNGCAP WHERE IDNHACUNGCAP='" + tbidnhacungcap.Text + "'";
+                command.ExecuteNonQuery();
+                loaddata();
+                MessageBox.Show("Xóa thành công!");
+                cleartb();
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show("Có lỗi khi xóa dữ liệu." + ex.Message, "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Có lỗi không xác định." + ex.Message, "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                command = connection.CreateCommand();
+                command.CommandText = "SELECT * FROM NHACUNGCAP WHERE TENNHACUNGCAP LIKE '%" + tbtimkiem.Text + "%'";
+                DataTable tk = new DataTable();
+                adapter.SelectCommand = command;
+                tk.Clear();
+                adapter.Fill(tk);
+                dataGridView1.DataSource = tk;
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show("Có lỗi khi tìm kiếm dữ liệu." + ex.Message, "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Có lỗi không xác định." + ex.Message, "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+    }
+}
